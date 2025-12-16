50f9b2915c5083f34ccfa489fd73ca2eed3ee4fa
allseenn <allseen@live.ru>
2025-12-16 11:38:42 +0000

    +curtain

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.indicator').forEach(item => {
+    const overlay = document.createElement('div');
+    overlay.className = 'ipc-overlay';
+    overlay.textContent = 'Перейти';
+    item.append(overlay);
+
+    item.addEventListener('mouseenter', () => {
+        item.classList.add('hovered');
+    });
+
+    item.addEventListener('mouseleave', () => {
+        item.classList.remove('hovered');
+    });
+});
